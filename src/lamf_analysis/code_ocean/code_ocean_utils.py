d3d248e932adf729caf7d0f53edd6e35f8bda777
Jinho Kim <jinho.kim@alleninstitute.org>
2025-09-29 18:20:29 +0000

    adding get_session_infos_from_docdb

--- src/lamf_analysis/code_ocean/code_ocean_utils.py
+++ src/lamf_analysis/code_ocean/code_ocean_utils.py
@@ -1,28 +1,46 @@
-
-import os
-import yaml
-import requests
-from typing import Optional, Union
-from pathlib import Path
+import pandas as pd
+from aind_data_access_api.document_db import MetadataDbClient
 
 
+def get_session_infos_from_docdb(subject_id, docdb_api_client=None, data_type='multiplane-ophys'):
+    if docdb_api_client is None:
+        API_GATEWAY_HOST = "api.allenneuraldynamics.org"
+        DATABASE = "metadata_index"
+        COLLECTION = "data_assets"
+        docdb_api_client = MetadataDbClient(
+                host=API_GATEWAY_HOST,
+                database=DATABASE,
+                collection=COLLECTION,
+            )
 
-def attach_assets(data: Union[list, str, Path], api_secret_name = "CODEOCEAN_TOKEN"):
-    """
+    query = {"subject.subject_id": subject_id, "data_description.data_level": "raw"}
+    subject_response = docdb_api_client.retrieve_docdb_records(
+                    filter_query=query,                
+                    )
+    session_infos = pd.DataFrame()
+    for response in subject_response:
+        # schema_version = response['schema_version']
+        # if schema_version == '1.1.1': # '1.1.1' and '1.0.2' tested
+        if response['name'].startswith(data_type):
+            acquisition_date = response['session']['session_start_time'][:10]
+            session_name = subject_id + "_" + acquisition_date
+            session_type = response['session']['session_type']
+            reward_consumed = response['session']['reward_consumed_total']
+            rig_id = response['session']['rig_id']
+            data_asset_name = response['name']
+            temp_info = {"acquisition_date": acquisition_date,
+                        "session_type": session_type,
+                        "reward_consumed": reward_consumed,
+                        "rig_id": rig_id,
+                        "session_name": session_name,
+                        "raw_asset_name": data_asset_name,
+                        }
+            session_infos = pd.concat([session_infos, pd.DataFrame(temp_info, index=[0])], ignore_index=True)
+            # else:
+            #     print(f"Schema version {schema_version} not handled.")
     
-    "API_SECRET"
-    """
+    session_infos.sort_values(by='acquisition_date', inplace=True)
+    session_infos.reset_index(drop=True, inplace=True)
+    session_infos['session_type_exposures'] = session_infos.groupby('session_type').cumcount() + 1
 
-    if isinstance(data, (str, Path)):
-        with open(data, mode="r") as f:
-            data = yaml.safe_load(f)
-    elif isinstance(data, list):
-        for d in data:
-            assert isinstance(d, dict), "All elements of the list must be dictionaries"
-
-    url = f'https://codeocean.allenneuraldynamics.org/api/v1/capsules/{os.getenv("CO_CAPSULE_ID")}/data_assets'
-    headers = {"Content-Type": "application/json"} 
-    auth = ("'" + os.getenv(api_secret_name), "'")
-    print(data)
-    response = requests.post(url=url, headers=headers, auth=auth, json=data)
-    print(response.text)
+    return session_infos
