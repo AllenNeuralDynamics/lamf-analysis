d16237463f668ab1b5ad8528115a0f82613d3a97
Jinho Kim <jinho.kim@alleninstitute.org>
2026-06-16 17:45:16 +0000

    using s3_path for running roicat_utils

--- src/lamf_analysis/code_ocean/s3_utils.py
+++ src/lamf_analysis/code_ocean/s3_utils.py
@@ -1,11 +1,15 @@
 import s3fs
 import json
 
-def list_files_from_s3_location(s3_path: str):
+def list_files_from_s3_location(s3_path: str, matched_string: str = None):
     fs = s3fs.S3FileSystem(anon=False)
     if not s3_path.startswith("s3://"):
         s3_path = "s3://" + s3_path
-    return [p for p in fs.glob(f"{s3_path}/**") if fs.isfile(p)]
+    files = [f's3://{p}' for p in fs.glob(f"{s3_path}/**") if fs.isfile(p)]
+    if matched_string is not None:
+        matched_string = matched_string.lower()
+        files = [f for f in files if matched_string in f.lower()]
+    return files
 
 
 def read_json_from_s3(s3_path: str):
